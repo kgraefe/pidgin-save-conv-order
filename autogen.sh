d7e5e29d5afab7470640b6ad100182fbaad21aff
Konrad Gräfe <konradgraefe@aol.com>
2016-02-03 14:05:56 +0100

    Add script for generation of configure.ac

--- autogen.sh
+++ autogen.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 set +x
 
-test -f VERSION || exit
-test -f configure.ac.in || exit
 test -f Makefile.am.in || exit
 test -f COPYING || exit
 
-languages=""
-for f in po/*.po; do
-	test -f $f && languages="$languages $(basename $f .po)"
-done
+./scripts/gen-configure_ac.sh || exit 1
 
 headers=""
 for f in src/*.h; do
 	test -f $f && headers="$headers $f"
 done
-
-sed \
-	-e "s/@@VERSION@@/$(cat VERSION)/" \
-	-e "s/@@LANGUAGES@@/$(echo $languages)/" \
-configure.ac.in >configure.ac || exit 1
 
 sed \
 	-e "s#@@HEADERFILES@@#$(echo $headers)#" \
