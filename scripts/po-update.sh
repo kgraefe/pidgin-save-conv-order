069b0c7af3a6f7d250a86d2edf115db06934994c
Konrad Gräfe <konradgraefe@aol.com>
2016-02-04 10:46:28 +0100

    po-update.sh: Add template name

--- scripts/po-update.sh
+++ scripts/po-update.sh
@@ -4,7 +4,7 @@ set +x
 cd "$(dirname "$0")/../po"
 
 echo "Updating POT template..."
-intltool-update -po || exit 1
+intltool-update --pot --gettext-package=pidgin-save-conv-order || exit 1
 
 for f in *.po; do
 	echo "Checking ${f%.po} language file..."
